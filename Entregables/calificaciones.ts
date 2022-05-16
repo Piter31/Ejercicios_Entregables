bfc684edd1f01973b78ff936118b78394faafba7
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-05-16 18:41:39 -0300

    nuevo repo...

---
+++ Entregables/calificaciones.ts
@@ -0,0 +1,17 @@
+
+
+let nombreAlumno: string;
+let nPractica, nProblema, nTeoria: number;
+
+nombreAlumno = string(pompt("Ingresar Nombre del Alumno: "));
+console.log("Nombre del Alumno: ", nombreAlumno);
+
+
+if (nPractica >= 0 && nPractica <= 10) {
+}
+
+if (nProblema >= 0 && nProblema <= 10) {
+}
+
+if (nTeoria >= 0 && nTeoria <= 10) {
+}
