9b9ac5c65e78b57ee4ea661334c89c24aca843af
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-05-17 00:00:53 -0300

    ultimos cambios..

--- Entregables/calificaciones.ts
+++ Entregables/calificaciones.ts
@@ -1,13 +1,22 @@
-
-let nAlumno: string(prompt ("Ingresar Nombre del Alumno: "));
+let nAlumno: string = String(prompt("Ingresar Nombre del Alumno"));
 let notaFinal: number;
 
+while (nAlumno !== " ") {
+  let nPractica: number = Number(prompt("Ingresar nota de Practica "));
+  let nProblema: number = Number(prompt("Ingresar nota de Problema "));
+  let nTeorica: number = Number(prompt("Ingresar nota de Teorica "));
 
-while (nAlumno </>= " ") {
-  let nPractica: number = Number(prompt("Ingresar nota de Practica: "));
-  let nProblema: number = Number(prompt("Ingresar nota de Problema: "));
-  let nTeorica: number = Number(prompt("Ingresar nota de Teorica: "));
-}
-  if (nPractica >= 0 && nPractica <= 10) {
+  if (
+    nPractica >= 0 &&
+    nPractica <= 10 &&
+    nProblema >= 0 &&
+    nProblema <= 10 &&
+    nTeorica >= 0 &&
+    nTeorica <= 10
+  ) {
+    notaFinal = Number(nPractica * 0.1 + nProblema * 0.5 + nTeorica * 0.4);
+    console.log("Alumno: " + nAlumno, "su nota final: " + notaFinal);
+  } else {
+    console.log("erro!! (verificar notas)");
   }
-
+}
