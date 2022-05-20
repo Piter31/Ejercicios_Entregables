dbd1e52315216686bad7a2d29d91e9b1a1309eb5
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-05-20 11:43:49 -0300

    agregando codigo

--- Entregables/calificaciones.ts
+++ Entregables/calificaciones.ts
@@ -4,27 +4,25 @@ let nAlumno: string = String(prompt("Ingresar Nombre del Alumno"));
 let notaFinal: number;
 
 while (nAlumno !== "") {
-  let nPractica: number = Number(prompt("Ingresar nota de Practica "));
-  let nProblema: number = Number(prompt("Ingresar nota de Problema "));
-  let nTeorica: number = Number(prompt("Ingresar nota de Teorica "));
+	let nPractica: number = Number(prompt("Ingresar nota de Practica "));
+	let nProblema: number = Number(prompt("Ingresar nota de Problema "));
+	let nTeorica: number = Number(prompt("Ingresar nota de Teorica "));
 
-  if (
-    nPractica > 0 &&
-    nPractica <= 10 &&
-    nProblema > 0 &&
-    nProblema <= 10 &&
-    nTeorica > 0 &&
-    nTeorica <= 10
-  ) {
-    //muestra el resultado final
-    notaFinal = Number(nPractica * 0.1 + nProblema * 0.5 + nTeorica * 0.4);
-    console.log("Alumno: " + nAlumno, "su nota final: " + notaFinal);
-    break;
-  } else {
-    console.log("error!! (Revisar notas)");
-    break;
-  }
-  let nAlumno: string = String(prompt("Ingresar Nombre del Alumno"));
+	if (
+		nPractica > 0 &&
+		nPractica <= 10 &&
+		nProblema > 0 &&
+		nProblema <= 10 &&
+		nTeorica > 0 &&
+		nTeorica <= 10
+	) {
+		//muestra el resultado final
+		notaFinal = Number(nPractica * 0.1 + nProblema * 0.5 + nTeorica * 0.4);
+		console.log("Alumno: " + nAlumno, "su nota final: " + notaFinal);
+	} else {
+		console.log("error!! (Revisar notas)");
+	}
+	let nAlumno: string = String(prompt("Ingresar Nombre del Alumno"));
 }
 
 //NOTA: cada ves que guardo, el mismo CodeSandbox me lo desidenta.
