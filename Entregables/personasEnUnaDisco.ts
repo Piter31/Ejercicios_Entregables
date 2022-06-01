44a13c0b508793a99ba767edbae80135f4124561
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-06-01 19:41:12 -0300

    disco

---
+++ Entregables/personasEnUnaDisco.ts
@@ -0,0 +1,3 @@
+//cantida de personas menores de 21 años
+//personas mayores o igual a 21 años
+//
--- Entregables/sumadearreglos.ts
+++ Entregables/sumadearreglos.ts
@@ -8,27 +8,25 @@ let sumaArreglos: number;
 let indice: number;
 
 //primer grupo de numeros...
-for (indice = 0; indice < 6; ) {
-	num1 = Number(
-		prompt("Ingresar numeros para el grupo 1, posicion: " + indice)
-	);
-	grupo1Arreglo[indice] = num1;
-	console.log("Posicion grupo 1: ", indice, "Numero: " + num1);
-	indice++;
+for (indice = 0; indice < 6; indice++) {
+  num1 = Number(
+    prompt("Ingresar numeros para el grupo 1, posicion: " + indice)
+  );
+  grupo1Arreglo[indice] = num1;
+  console.log("Posicion grupo 1: ", indice, "Numero: " + num1);
 }
 
 // segundo grupo de numero...
-for (indice = 0; indice < 6; ) {
-	num2 = Number(
-		prompt("Ingresar numeros para el grupo 2, posicion: " + indice)
-	);
-	grupo2Arreglo[indice] = num2;
-	console.log("Posicion grupo 2: ", indice, "Numero: " + num2);
-	indice++;
+for (indice = 0; indice < 6; indice++) {
+  num2 = Number(
+    prompt("Ingresar numeros para el grupo 2, posicion: " + indice)
+  );
+  grupo2Arreglo[indice] = num2;
+  console.log("Posicion grupo 2: ", indice, "Numero: " + num2);
 }
+
 //suma de los dos arreglos (Grupo 1 + Grupo 2)
-for (indice = 0; indice < 6; ) {
-	sumaArreglos = grupo1Arreglo[indice] + grupo2Arreglo[indice];
-	console.log("Suma de la posicion: ", indice, "igual a: " + sumaArreglos);
-	indice++;
+for (indice = 0; indice < 6; indice++) {
+  sumaArreglos = grupo1Arreglo[indice] + grupo2Arreglo[indice];
+  console.log("Suma de la posicion: ", indice, "igual a: " + sumaArreglos);
 }
