2b8c32eca653b47750150607d4eb8049ec262dd8
Pedro M Ibarra <103613346+Piter31@users.noreply.github.com>
2022-06-03 06:06:37 -0300

    falta terminar

--- Entregables/personasEnUnaDisco.ts
+++ Entregables/personasEnUnaDisco.ts
@@ -1,3 +1,19 @@
-//cantida de personas menores de 21 años
-//personas mayores o igual a 21 años
+// EJEMPLO: let arregloClientes : number[] = new Array(30);
+
+//cantida de personas menores de 21 años.
+//personas mayores o igual a 21 años.
+//cuantas personas en total.
+
+let persona: string;
+let edad: number;
+
 //
+persona = String(prompt("Ingresar nombre de la persona "));
+edad = Number(prompt("Ingresar edad de la persona "));
+if (edad > 21) {
+  console.log("Nombre de la Persona: " + persona, "edad: " + edad);
+}
+
+//function Azar(tope: number) {
+//  return Math.floor(Math.random() * tope);
+//}
